cfbfb9a1660b5dfec91956bdc5da959ce7d75a3d
Orie Steele <orie@ohs.io>
2017-10-09 18:47:09 -0500

    update functions to prevent errors

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,40 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const querystring = require("querystring");
 const url = require("url");
-const cors = require('cors')({ origin: true });
-const user_functions = require('./src');
-const env = require('./environment.node');
+const cors = require("cors")({ origin: true });
+const user_functions = require("./src");
+const env = require("./environment.node");
 
 exports.echo = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-        let response = user_functions['echo']({
-            name: 'echo',
-            query: querystring.parse(url.parse(req.url).query),
-            body: req.body,
-            env
-        });
-        if (!response.redirect) {
-            res.status(response.status).json(response);
-        } else {
-            res.redirect(response.redirect)
-        }
+  cors(req, res, () => {
+    user_functions["echo"]({
+      name: "echo",
+      query: querystring.parse(url.parse(req.url).query),
+      body: req.body,
+      env
+    }).then(response => {
+      if (!response.redirect) {
+        res.status(response.status).json(response);
+      } else {
+        res.redirect(response.redirect);
+      }
     });
+  });
 });
 
 exports.token = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
-        let response = user_functions['token']({
-            name: 'token',
-            query: querystring.parse(url.parse(req.url).query),
-            body: req.body,
-            env
-        });
-        if (!response.redirect) {
-            res.status(response.status).json(response);
-        } else {
-            res.redirect(response.redirect)
-        }
+  cors(req, res, () => {
+    user_functions["token"]({
+      name: "token",
+      query: querystring.parse(url.parse(req.url).query),
+      body: req.body,
+      env
+    }).then(response => {
+      if (!response.redirect) {
+        res.status(response.status).json(response);
+      } else {
+        res.redirect(response.redirect);
+      }
     });
+  });
 });
-
--- functions/src/echo/index.js
+++ functions/src/echo/index.js
@@ -1,10 +1,13 @@
+const _ = require("lodash");
 
-module.exports = (functionParams) => {
-    // normally all work would be done here.
-    // redirect, body and status are used handled the same way by server and cloud function
-    return {
-        status: 200,
-        body: functionParams,
-        redirect: null
-    }
-}
+module.exports = functionParams => {
+  // normally all work would be done here.
+  // redirect, body and status are used handled the same way by server and cloud function
+  return new Promise((resolve, reject) => {
+    resolve({
+      status: 200,
+      body: _.omit(functionParams, "env"),
+      redirect: null
+    });
+  });
+};
